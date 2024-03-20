7f77a23f5ca2092e852af6654fa1bb172e82c900
Shafkat26 <49305859+Shafkat26@users.noreply.github.com>
2024-03-20 00:38:23 -0400

    Add files via upload

--- epic6/epic6/epic6/settings.py
+++ epic6/epic6/epic6/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e&6%k)!m10q(ll^x66ak9(voxe3l@*(_qfv6(ds#&yj_^_y*aj'
+SECRET_KEY = 'django-insecure-jd4gnj-jdh@%voselnq+&wz7@rdgj94x^!0wpn&_1#@jmn#9k0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registration'
+    'user'
 ]
 
 MIDDLEWARE = [
