cf4f6fd6fc24f3c4a213258f11fb24a02da1a697
Shafkat26 <49305859+Shafkat26@users.noreply.github.com>
2024-03-20 00:39:21 -0400

    Delete epic6/epic6/registration directory

--- epic6/epic6/registration/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UserAccount
-
-class UserRegistrationForm(forms.ModelForm):
-    class Meta:
-        model = UserAccount
-        fields = ['username', 'email', 'password']
--- epic6/epic6/registration/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import register_user, registration_success
-
-urlpatterns = [
-    path('register/', register_user, name='register'),
-    path('registration-success/', registration_success, name='registration_success'),
-]
