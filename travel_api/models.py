30752d4ecbaa9a6e6cadb28a6bc8e05857d2f0f1
steamypotato <hoopahydra@gmail.com>
2024-03-25 21:22:03 -0400

    Better HTML for the booking

--- travel_api/models.py
+++ travel_api/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.db.models.functions import Now
 
@@ -9,7 +11,7 @@ class RoomType(models.TextChoices):
 
 
 class Hotel(models.Model):
-    name = models.CharField(max_length=200,default="[Hotel name]")
+    name = models.CharField(max_length=200, default="[Hotel name]")
     address = models.CharField(max_length=200, default="[Hotel address]")
     room_type = models.CharField(max_length=20, choices=RoomType.choices, default=RoomType.SINGLE)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
@@ -22,22 +24,25 @@ class Hotel(models.Model):
 class Flight(models.Model):
     name = models.CharField(max_length=200, default="[Flight name]")
     departure_location = models.CharField(max_length=200, default="[Departure Location]")
-    arrival_location = models.CharField(max_length=200,default="[Arrival location]")
-    departure_time = models.DateTimeField(null=True,blank=True)
-    arrival_time = models.DateTimeField(null=True,blank=True)
+    arrival_location = models.CharField(max_length=200, default="[Arrival location]")
+    departure_time = models.DateTimeField(default=datetime.now, blank=True)
+    arrival_time = models.DateTimeField(default=datetime.now, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=1000.0)
 
     def __str__(self):
         return self.name
+
+
 class PackageDetails(models.Model):
     country = models.CharField(max_length=70, blank=False, default='[Country]')
     price = models.FloatField(default=0)
     rating = models.FloatField(default=0)
-    amenities = models.CharField(max_length=100, blank=False, default='All-inclusive,Pool,Sauna,Backrooms')
+    amenities = models.CharField(max_length=100, blank=False, default='[Activity1],[Activity2],[Activity3]')
     image_path = models.CharField(max_length=100, blank=False, default="quinta.png")
 
+
 class Package(models.Model):
-    name = models.CharField(max_length=30,default='[default name]')
-    hotel = models.OneToOneField(Hotel,on_delete=models.CASCADE,null=True)
-    flight = models.OneToOneField(Flight,on_delete=models.CASCADE,null=True)
-    _package = models.OneToOneField(PackageDetails,on_delete=models.CharField,null=True)
+    name = models.CharField(max_length=30, default='[default name]')
+    hotel = models.OneToOneField(Hotel, on_delete=models.CASCADE, null=True)
+    flight = models.OneToOneField(Flight, on_delete=models.CASCADE, null=True)
+    _package = models.OneToOneField(PackageDetails, on_delete=models.CharField, null=True)
