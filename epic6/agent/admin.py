b5d99bd17ac3d133958064b78d3020c8306d6c00
Shafkat26 <49305859+Shafkat26@users.noreply.github.com>
2024-03-28 00:41:42 -0400

    Delete epic6 directory

--- epic6/agent/admin.py
+++
@@ -1,8 +0,0 @@
-# agents/admin.py
-from django.contrib import admin
-from .models import Agent
-
-@admin.register(Agent)
-class AgentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone')
-    search_fields = ('name', 'email', 'phone')
